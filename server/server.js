b9269ae03fb8182a613c87b305bd813f94b6d265
Gregor Ojstersek <grega.ojstersek@gmail.com>
2018-12-29 19:53:43 +0100

    Add newMessage and createMessage events

--- server/server.js
+++ server/server.js
@@ -14,9 +14,19 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
+    socket.emit('newMessage', {
+        from: 'gregor',
+        text: 'Hey',
+        createdAt: '2018-10-10'
+    });
+
+    socket.on('createMessage', (message) => {
+        console.log('createMessage', message);
+    });
+
     socket.on('disconnect', () => {
         console.log('User was disconnected from server');
-    })
+    });
 })
 
 server.listen(port, () => {
